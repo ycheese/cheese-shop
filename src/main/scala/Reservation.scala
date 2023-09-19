43a60d612504ab04e59ae66b7cd93699277c64d9
CHEESE <81912261+ychxexn@users.noreply.github.com>
2023-09-19 15:38:45 +0900

    chore: resources 디렉토리 내 파일 삭제

--- src/main/scala/Reservation.scala
+++ src/main/scala/Reservation.scala
@@ -1,7 +1,20 @@
+import ujson.Value
+import zio.json.{DecoderOps, DeriveJsonCodec, JsonCodec}
+
 case class Reservation(id: Int,
                        user: User,
                        date: String,
                        time: String,
                        guests: String,
                        isClosed: Boolean = false,
-                       isPaied: Boolean = false)
+                       isPaied: Boolean = false) {
+  def close() = Reservation(this.id, this.user, this.date, this.time, this.guests, true, this.isPaied)
+}
+
+object Reservation {
+  implicit val codec: JsonCodec[Reservation] = DeriveJsonCodec.gen[Reservation]
+
+  def parseJson(value: Value): Either[String, List[Reservation]] = for {
+    json <- value.toString().fromJson[List[Reservation]]
+  } yield json
+}
